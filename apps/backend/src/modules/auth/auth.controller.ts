5041db0dd8276cc88bad0085455688f8866dbee1
abdellah <abdo.abdell.2000@gmail.com>
2025-12-30 05:53:29 +0100

    SHP-51: documentation swagger for api of the authentication methodes: login and register in controller

--- apps/backend/src/modules/auth/auth.controller.ts
+++ apps/backend/src/modules/auth/auth.controller.ts
@@ -5,7 +5,7 @@ import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
- import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+ import { ApiBearerAuth, ApiTags, ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
  interface AuthenticatedRequest extends Request {
     user: {
@@ -15,25 +15,38 @@ import { JwtAuthGuard } from './guards/jwt-auth.guard';
     }
  }
 
-
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
-
+    @ApiOperation({ summary: 'Créer un compte'})
+    @ApiResponse({ status: 201, description: 'User crée avec succès!'})
+    @ApiResponse({ status: 409, description: 'Email déjà utilisé !'})
+    @ApiBody({ type: CreateUserDto})
     @Post('register')
     async register(@Body() createUserDto: CreateUserDto) {
         return this.authService.register(createUserDto);
     }
 
+
+
+    @ApiOperation({ summary: 'Authentification : '})
+    @ApiResponse({ status: 200, description: 'JWT retourné ! '})
+    @ApiResponse({ status: 401, description: 'Identifiants invalides !'})
+    @ApiBody({ type: LoginDto})
     @Post('login')
     async login(@Body() loginDto: LoginDto) {
         const accessToken = await this.authService.login(loginDto);
         return { accessToken };
     }
 
+
+
     @ApiBearerAuth()
+    @ApiOperation({ summary: 'Récupérer le profil user connecté : '})
+    @ApiResponse({ status: 200, description: 'User profil ! '})
+    @ApiResponse({ status: 401, description: 'Non authentifié !'})
     @UseGuards(JwtAuthGuard)
     @Get('my-profile')
     async getProfile(@Req() req: AuthenticatedRequest) {
