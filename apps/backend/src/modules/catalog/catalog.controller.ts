1045a56662de3914c079b61b164da771e26d0025
abdellah <abdo.abdell.2000@gmail.com>
2026-01-10 15:15:35 +0100

    SHP-28: implementing interceptors of cache in core because it's not depending on catalog module, and use it in controller for caching the get request method

--- apps/backend/src/modules/catalog/catalog.controller.ts
+++ apps/backend/src/modules/catalog/catalog.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, HttpCode, HttpStatus, Query } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Query, UseInterceptors } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { CacheInterceptor } from '../../core/interceptors/cache.interceptor';
 import { CatalogQueryDto } from './dto/catalog-query.dto';
 
 @ApiTags('Catalog')
 @Controller('catalog')
+@UseInterceptors(CacheInterceptor)
 export class CatalogController {
   @ApiOperation({ summary: 'Lister les produits publics' })
   @ApiResponse({ status: 200, description: 'Liste des produits' })
