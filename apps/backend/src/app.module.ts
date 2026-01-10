6af5069ea4465f997b4dd62515176e88c0140c2e
abdellah <abdo.abdell.2000@gmail.com>
2026-01-10 14:49:04 +0100

    SHP-26: creation of catalog module and his controller

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { CatalogModule } from './modules/catalog/catalog.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
-    AuthModule
+    AuthModule,
+    CatalogModule
   ],
 })
 export class AppModule {}
